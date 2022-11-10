4a665ff25413c92e01f4683d950f9cd9a864e70d
NeonOrbit <acc.neonic@gmail.com>
2022-11-10 10:04:20 +0000

    Bump version to 5.5.1

--- patch-loader/src/main/java/org/lsposed/lspatch/loader/LSPApplication.java
+++ patch-loader/src/main/java/org/lsposed/lspatch/loader/LSPApplication.java
@@ -60,7 +60,7 @@ public class LSPApplication {
 
     @SuppressWarnings("unused")
     public static void onLoad() {
-        Log.i(TAG, "MRV-v5.5.0");
+        Log.i(TAG, "MRV-v5.5.1");
         Log.i(TAG, "Bootstrap: " + ActivityThread.currentProcessName());
         if (isIsolated()) {
             Log.i(TAG, "Skip isolated process");
